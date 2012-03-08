9c195b8bd2c4969c09665a935d48b4de84bf561e
Peter Casinelli <petercasinelli@Peters-MacBook-Pro.local>
2012-03-08 11:56:34 -0500

    Updated assets from Ron Radu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 function toggleExpand(id) {
 	$('#item'+id).slideToggle();
 	if($('#expand'+id).html()=='Collapse') {
-		$('#expand'+id).html('Expand');
+		$('#expand'+id).html('Expand Profile');
 	} else {
 		$('#expand'+id).html('Collapse');
 	}
 }
+
+$(function() {
+	$('#edit-profile [title]').tipsy({trigger:'focus', gravity:'w'});
+});
