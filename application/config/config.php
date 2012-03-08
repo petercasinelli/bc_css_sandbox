8093a8f7afaad847be07f658eaaa91eedb5dba2b
Peter Casinelli <petercasinelli@Peters-MacBook-Pro.local>
2012-03-08 11:49:43 -0500

    Took out student subclass prefix. Only use MY_

--- application/config/config.php
+++ application/config/config.php
@@ -108,7 +108,7 @@ $config['enable_hooks'] = FALSE;
 */
 
 $config['subclass_prefix'] = 'MY_';
-$config['subclass_prefix'] = 'STUDENT_';
+
 
 
 /*
@@ -182,7 +182,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
@@ -193,7 +193,7 @@ $config['log_threshold'] = 0;
 | application/logs/ folder. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = '/application/logs/log.txt';
 
 /*
 |--------------------------------------------------------------------------
