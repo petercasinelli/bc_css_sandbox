3dae9d3da35285f784d801d7a514da5d3b1c7ba1
Peter Casinelli <pcas00@gmail.com>
2012-03-08 13:16:19 -0500

    Updated header title for BC Skills

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Technical Talent Directory - Boston College Computer Science Society</title>
+	<title>BC Skills - Boston College Computer Science Society</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/reset.css" type="text/css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" type="text/css">
