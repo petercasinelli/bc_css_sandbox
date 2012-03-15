335231f9991674da442b88886cd85b9492616b68
Ron Radu <ron@ronradu.com>
2012-03-15 19:46:13 -0400

    Removed search bar from all left navs

--- application/views/student/includes/leftsidebar.php
+++ application/views/student/includes/leftsidebar.php
@@ -1,11 +1,5 @@
 <div id="left-column">
 		<a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>assets/images/logo.png"></a>
-			
-		<?php echo form_open('student/search/', 'id="search"'); ?>
-		<form action="" method="" id="search">
-			<!--no space between inputs for presentational reasons-->
-			<input type="text" name="query" placeholder="Search names, skills, majors..."><input type="submit" name="submit" value="Search">
-		<?php echo form_close(); ?>
 		
 		Signed in as <b><?php echo $student_logged_in->first . ' ' . $student_logged_in->last ?></b>. <?php echo anchor('authentication/student/logout', "Sign Out"); ?>.
 
