202a56dd576b367ce30c58b5104d498efae9f5d7
Peter Casinelli <pcas00@gmail.com>
2012-03-23 13:03:53 -0400

    Added Google Analytics to footer.php file

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -4,6 +4,20 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>assets/js/script.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>assets/js/tipsy.js" type="text/javascript"></script>
+<!-- Google Analysitcs -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30238728-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 </body>
 </html>
