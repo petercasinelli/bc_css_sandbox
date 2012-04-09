5f4f3a321de59f1b16814dfecb5bca52bc14454a
Peter Casinelli <pcas00@gmail.com>
2012-04-09 16:57:05 -0400

    Updated calendar view for upcoming CSS events

--- application/views/student/calendar.php
+++ application/views/student/calendar.php
@@ -7,38 +7,25 @@
 		
 		<div class="item non-user-item text-item">
 			<hgroup>
-				<h2>CSS Special Event</h1>
-				<h3>March 15th, 2012 @ 4pm-5pm</h2>
+				<h2>Building High Performance Web Applications</h1>
+				<h3>April 12th, 2012 @ 5pm-6pm</h2>
 			</hgroup>
-			<p>You are invited to a CSS special event. We will be unveiling a secret project and discussing our plans for the rest of the semester. We hope to see you there! Pizza and drinks will be served.</p>
+			<p>Guest lecture by Jeff Whelpley.</p>
+			<p>In a world where Google search results load instantly and hand held devices get more powerful by the day, 
+				the bar for website performance is at unprecedented levels. It isn't enough that an application has cool 
+				features. It has to be fast. Really fast. If you plan on working in the software industry, you need to 
+				understand the basic principles of performance on the web and how to utilize tools and techniques to ensure 
+				the products you create will meet customer expectations for speed. In this presentation, Jeff will introduce 
+				the high level concepts, get into some real world examples, go over the process of optimization and provide a 
+				vision for high performance web applications of the future.</p>
 		</div>
 		
 		<div class="item non-user-item text-item">
 			<hgroup>
-				<h2>That Great Idea Isn't Yours Unless You Own It: Intellectual Property Protection for the Uninitiated</h1>
-				<h3>March 22nd, 2012 @ 5pm-6pm</h2>
+				<h2>Android Development Workshop</h1>
+				<h3>Apr 14, 2012 @ 12pm-3pm</h2>
 			</hgroup>
-			<p>Guest lecture by Matt Lowrie.</p>
 			
-			<p>Mr. Lowrie will discuss the forms of intellectual property available to new businesses, 
-				how to protect ideas, and how you can blow it. He will also present a fair number of ridiculous patents, such as one
-				 on a centrifugal force birth assist device (think about it).</p>
-				 
-			<p>Mr. Lowrie is a partner with Foley & Lardner LLP, where he is chair of the firm’s Intellectual Property Litigation
-				  Practice. In addition to his to his experience as a litigator and counselor, Mr. Lowrie has a deep
-				   technical background: He attended the University of Illinois, where he completed the course work for a Ph.D. in
-				    electrical engineering and received an M.S. in electrical engineering, and a B.S. in computer engineering.</p>
-		</div>
-		
-		<div class="item non-user-item text-item">
-			<hgroup>
-				<h2>Ruby on Rails: Development process from vision to release</h1>
-				<h3>March 29th, 2012 @ 4pm-5pm</h2>
-			</hgroup>
-			<p>Chris Paul is the VPE at BlueLeaf, a Boston-area startup company.  He is a software development veteran
-				 with 30+ years of experience.</p>
-				 
-			<p>At BlueLeaf he works with Rails on a daily basis and is in charge of 
-				 the development team.This lecture is a technical talk about how to get started in Rails and what to do
-				  with your first app.</p>
+			<p>John Bacon (BC '12) will be conducting a workshop on how to build mobile apps for the Android platform.</p>
+
 		</div>
