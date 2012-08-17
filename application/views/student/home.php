e41eae19df61762f32c11a49ad09e155706a4911
Peter Casinelli <pcas00@gmail.com>
2012-08-17 09:10:16 -0400

    Updated view for logged in students

--- application/views/student/home.php
+++ application/views/student/home.php
@@ -6,24 +6,75 @@ $this->load->view('student/includes/header');
 
 <section>
     <div class="grid">
+        <div class="quarter">
+            <section>
+            <header>
+                <h1>Complete Your Profile</h1>
+            </header>
+            </section>
+        </div>
         <div class="three-quarters">
             <section>
+                Form goes here
+            </section>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="grid">
+        <div class="half">
+            <section>
                 <header>
-                    <h1>Three-quarters</h1>
+                    <h1>What's New</h1>
                 </header>
                 <p>
-                    3/4 test
+                   New updates here
                 </p>
             </section>
         </div>
-        <div class="quarter">
+        <div class="half">
             <section>
                 <header>
-                    <h1>Quarter</h1>
+                    <h1>New Students</h1>
                 </header>
+                <section class="listing">
                 <p>
-                    1/4 test
+                    Student 1
+                    <br />
+                    <b>Bio goes here</b>
                 </p>
+            </section>
+                <section class="listing">
+                    <p>
+                        Student 2
+                        <br />
+                        <b>Bio goes here</b>
+                    </p>
+                </section>
+                <?php echo anchor('student/view_all', 'View All Students'); ?>
+
+            </section>
+            <br />
+            <section>
+                <header>
+                    <h1>New Teams</h1>
+                </header>
+                <section class="listing">
+                    <p>
+                        Team 1
+                        <br />
+                        <b>Description goes here</b>
+                    </p>
+                </section>
+                <section class="listing">
+                    <p>
+                        Team 2
+                        <br />
+                        <b>Description goes here</b>
+                    </p>
+                </section>
+                <?php echo anchor('/team', 'View All Teams'); ?>
             </section>
         </div>
     </div>
