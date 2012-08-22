6d756fe8b28af9f436bab9cd0c190cf21e943db1
Alan Lin <linpd2@gmail.com>
2012-08-22 13:29:13 -0400

    refactored to show local image if exists

--- application/helpers/image_helper.php
+++ application/helpers/image_helper.php
@@ -4,18 +4,17 @@ function student_picture_src($student_id, $oauth_uid, $picture){
 	
 	$CI =& get_instance();
 	$CI->load->helper("path_helper");
+	$student_pic_url = student_picture_url();
 	
-	if($oauth_uid){
-		return "https://graph.facebook.com/". $oauth_uid . "/picture?type=large";
-	}else{
-		$student_pic_url = student_picture_url();
-		if($picture){
-			return $student_pic_url . $picture;
-		}else{
-			//return placeholder
-			return "http://i0.kym-cdn.com/photos/images/original/000/074/586/tumblr_l7l1nqA5va1qdsweso1_1280.jpg?1318992465";
-		}
-	}
+	if($picture):
+		return $student_pic_url . $picture;
+	else:
+		if($oauth_uid):
+			return "https://graph.facebook.com/". $oauth_uid . "/picture?type=large"; //high resolution
+		else:
+			return "http://i0.kym-cdn.com/photos/images/original/000/074/586/tumblr_l7l1nqA5va1qdsweso1_1280.jpg?1318992465"; //dummy image
+		endif;
+	endif;
 	
 }
 
