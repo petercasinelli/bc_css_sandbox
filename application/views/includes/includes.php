830a3c4b67265ac644f1bbed6ba764e5b3848176
Alan Lin <linpd2@gmail.com>
2012-08-25 11:41:12 -0400

    added autosuggest plugin

--- application/views/includes/includes.php
+++ application/views/includes/includes.php
@@ -3,6 +3,7 @@
 <head>
 	<title>BC Skills - Boston College Computer Science Society</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/autoSuggest.css" type="text/css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/reset.css" type="text/css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" type="text/css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/tipsy.css" type="text/css">
@@ -12,5 +13,4 @@
 	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>assets/images/apple-icon-72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>assets/images/apple-icon-114.png">
 </head>
-
 <body>
