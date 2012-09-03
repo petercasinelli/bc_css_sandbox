963dec95b84695b34f53fd1e226b111f6df4e59c
Peter Casinelli <pcas00@gmail.com>
2012-09-03 14:10:24 -0400

    Changes to edit profile view and student home

---
+++ application/views/student/tutorial.php
@@ -0,0 +1,15 @@
+<section>
+    <div class="grid">
+        <div id="slides">
+            <div class="slides_container">
+                <div>
+                    HAHAHA
+                </div>
+                <div>
+                    PETER
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
