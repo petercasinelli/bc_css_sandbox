fa63fef1c12ae280ac79405c5886b7ab095ebe27
Peter Casinelli <pcas00@gmail.com>
2012-09-06 17:36:30 -0400

    Reverting back to keera MY_Controller fix for student skills

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -11,6 +11,8 @@ class MY_Controller extends CI_Controller {
 		
 		$this->load->helper('authentication_helper');
 		if(!student_is_logged_in()):
+            $this->load->library('message');
+            $this->message->set('You must be logged in to access that page.', 'error', TRUE);
 			redirect('/');
 			exit(1);
 		endif;
@@ -18,11 +20,11 @@ class MY_Controller extends CI_Controller {
 		$this->current_student_id = $this->session->userdata('student_id');
 		//we need to use template pattern for this..
 		$this->current_student_info = $this->student_model->get_student($this->current_student_id);
-		/*$student_skills = $this->student_model->get_student_skills($this->current_student_id);
+		$student_skills = $this->student_model->get_student_skills($this->current_student_id);
         $this->current_student_info->skills = '';
         foreach($student_skills as $skill):
         	$this->current_student_info->skills = $this->current_student_info->skills . $skill->skill . ', ';
-        endforeach;*/
+        endforeach;
     }
 	
 }
