12754d721876d063fe54102298e8a1196c9604d4
Peter Casinelli <pcas00@gmail.com>
2012-09-06 17:34:54 -0400

    Fixed pagination bug for slider

--- application/views/student/home.php
+++ application/views/student/home.php
@@ -80,29 +80,7 @@ $data["jquery"] = "
 				play: 5000,
 				pause: 5000,
 				hoverPause: true,
-				animationStart: function(current){
-					$('.caption').animate({
-						bottom:-35
-					},100);
-					if (window.console && console.log) {
-                        // example return of current slide number
-                        console.log('animationStart on slide: ', current);
-                    };
-				},
-				animationComplete: function(current){
-					$('.caption').animate({
-						bottom:0
-					},200);
-					if (window.console && console.log) {
-                        // example return of current slide number
-                        console.log('animationComplete on slide: ', current);
-                    };
-				},
-				slidesLoaded: function() {
-					$('.caption').animate({
-						bottom:0
-					},200);
-				}
+				paginationClass: 'slide_pagination'
 			});
 		});
 ";
