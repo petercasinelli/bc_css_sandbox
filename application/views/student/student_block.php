dae3f7ea9a5e5e8188980337c1501aaf1e57105d
Peter Casinelli <pcas00@gmail.com>
2012-10-15 22:34:40 -0400

    Updated student block to make it smaller in order to fit more per page

--- application/views/student/student_block.php
+++ application/views/student/student_block.php
@@ -1,8 +1,9 @@
-<section class="listing">
+<div class="listing half" style="width:400px; height:220px;">
 			<header>
 				<?php $pic_src = student_picture_src($student->student_id, $student->oauth_uid, $student->picture); ?>
-				<img src="<?php echo $pic_src; ?>" width="25px" height="25px" class="float-left"/>
-                <hgroup class="float-left">
+				<img src="<?php echo $pic_src; ?>" width="50px" height="50px" class="float-left"/>
+
+                <hgroup style="float:left; margin:0px; width:340px;">
                     <h2><?php echo $student->first . ' ' . $student->last; ?></h2>
                     <h3><?php if($student->school_id) echo $student->school;
                         if($student->year) echo ' ' . $student->year;
@@ -11,44 +12,51 @@
                 </hgroup>
 			</header>
 
-			<div class="float-right">
-                <br />
-				<span class="social-links">
-					<?php
-					if (!empty($student->twitter)):
-					?>
-					<a href="http://www.twitter.com/<?php echo $student->twitter; ?>" class="twitter" title="Twitter" target="_blank"></a>
-					<?php endif;
-					if (!empty($student->facebook)):
-					?>
-					<a href="<?php echo $student->facebook; ?>" class="facebook" title="Facebook" target="_blank"></a>
-					<?php
-					endif;
-					if (!empty($student->linkedin)):
-					?>
-					<a href="<?php echo $student->linkedin; ?>" class="linkedin" title="LinkedIn" target="_blank"></a>
-					<?php
-					endif;
-					if (!empty($student->dribbble)):
-					?>
-					<a href="<?php echo $student->dribbble; ?>" class="dribbble" title="Dribbble" target="_blank"></a>
-					<?php
-					endif;
-					if (!empty($student->github)):
-					?>
-					<a href="<?php echo $student->github; ?>" class="github" title="Github" target="_blank"></a>
-					<?php
-					endif;
-					?>
-				</span>
-				<a href="mailto:<?php echo $student->email; ?>">Contact</a>
-			</div>
 			<hr>
+
 			<p>
                 <b>Skills</b>: <?php echo $student->skills; ?>
                 <br />
-				<b>Bio</b>: <?php echo $student->bio; ?>
+				<b>Bio</b>: <?php
+                if (strlen($student->bio) > 150)
+                    echo substr($student->bio,0,150) . '...' . anchor('/view/'.$student->student_id, 'Read More');
+                else
+                    echo $student->bio;
+                    ?>
+
 
 			</p>
-</section>
+    <div>
+        <br />
+				<span class="social-links">
+					<?php
+                    if (!empty($student->twitter)):
+                        ?>
+                        <a href="http://www.twitter.com/<?php echo $student->twitter; ?>" class="twitter" title="Twitter" target="_blank"></a>
+                        <?php endif;
+                    if (!empty($student->facebook)):
+                        ?>
+                        <a href="<?php echo $student->facebook; ?>" class="facebook" title="Facebook" target="_blank"></a>
+                        <?php
+                    endif;
+                    if (!empty($student->linkedin)):
+                        ?>
+                        <a href="<?php echo $student->linkedin; ?>" class="linkedin" title="LinkedIn" target="_blank"></a>
+                        <?php
+                    endif;
+                    if (!empty($student->dribbble)):
+                        ?>
+                        <a href="<?php echo $student->dribbble; ?>" class="dribbble" title="Dribbble" target="_blank"></a>
+                        <?php
+                    endif;
+                    if (!empty($student->github)):
+                        ?>
+                        <a href="<?php echo $student->github; ?>" class="github" title="Github" target="_blank"></a>
+                        <?php
+                    endif;
+                    ?>
+				</span>
+        <a href="mailto:<?php echo $student->email; ?>">Contact</a>
+    </div>
+</div>
 		
