125a941930b0d480b19108a53ac1f45a6b638fef
Peter Casinelli <pcas00@gmail.com>
2013-02-11 18:38:01 +0100

    Updated button and padding styles

--- application/views/home.php
+++ application/views/home.php
@@ -12,7 +12,7 @@ endif;
 
 <div class="container">
 
-    <div class="hero-unit" style="padding-top:5px; padding-bottom:25px; margin-top:10px; margin-bottom:15px;">
+    <div class="hero-unit" style="padding:10px 20px 20px 20px; margin-top:10px; margin-bottom:15px;">
 
         <h2>Start-Ups At Boston College</h2>
         <h4 style="color:#636363; text-transform: none;">Create an account and find a co-founder, team member, or a team in seconds!</h4>
@@ -37,7 +37,7 @@ endif;
         <div class="span6 hero-unit" style="padding:10px 20px 20px 20px;">
             <h2>BCVC Team Registration</h2>
             <p>Are you competing in the Boston College Venture Competition? Easily register on BC Skills, add your current team members, and start recruiting other team members. It only takes 30 seconds to set up a profile, team, and presence on BC Skills. Get started now!</p>
-            <p><a class="btn btn-primary .btn-large" href="#">Register for BCVC &raquo;</a></p>
+            <p><a class="btn .btn-large" href="#">Register for BCVC &raquo;</a></p>
         </div>
         <div class="span5 hero-unit" style="padding:10px 20px 20px 20px;">
             <h2>Testimonial</h2>
@@ -45,7 +45,7 @@ endif;
             <br />
             <img src="<?php echo base_url(); ?>assets/images/testimonials/keemon-testimonial.jpg" height="65" width="65" class="img-polaroid" style="float:left;" />
             <p><small>"BCSkills empowers startups and developers to connect seamlessly. Through my use of the service, I have been approached with numerous employment opportunities. A startup dress.me reached out to me through BCSkills, which led to valuable work experience..."</small></p>
-            <p><a class="btn btn-primary .btn-large" href="#">Read the Full Testimonial &raquo;</a></p>
+            <p><a class="btn .btn-large" href="#">Read the Full Testimonial &raquo;</a></p>
         </div>
     </div>
 
