d887d2f73c0deeaa577d03de9f1f9e49fe20ac30
Alan Lin <linpd2@gmail.com>
2013-03-17 15:57:57 -0400

    moved email to authen

--- application/helpers/authentication_helper.php
+++ application/helpers/authentication_helper.php
@@ -2,7 +2,31 @@
 
 function student_is_logged_in(){
 	$CI = & get_instance();
-	return $CI->session->userdata('student_id') ? TRUE: FALSE;
+	return $CI->session->userdata('student_id' ) ? TRUE: FALSE;
 }
 
-//end of authentication_helper.php
+class Authen
+{
+    public static function send_password($email, $new_password)
+    {
+        $CI = & get_instance();
+        
+        $CI->load->library('email');
+        $CI->email->set_newline("\r\n").
+        $CI->email->from('bccss.development@gmail.com', 'BC Skills');
+        $CI->email->to($email);
+        $CI->email->subject('BC Skills Password Reset');
+        $CI->email->message('Hello,
+
+        You recently requested to reset your password on BCSkills.com.
+
+        Here is your new password: ' . $new_password . '
+
+        Please let us know if you have any issues,
+
+        BC CSS Team');
+        
+        return $CI->email->send();
+    }
+}
+    //end of authentication_helper.php
