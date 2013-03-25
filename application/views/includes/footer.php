5f4c87b3c51fd9b6cb0c1c0099f3f8599e703255
Alan Lin <linpd2@gmail.com>
2013-03-24 23:02:05 -0400

    cleanup footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -3,20 +3,21 @@
         <h3>BC Skills is a <a href="https://github.com/pcas00/bc_css_sandbox" target="_blank">BC CSS project</a> with contributions from Alan Lin, Ron Radu, Matt Keemon, Kevin Lamb, and Peter Casinelli.</h3>
     </div>
 </section>
+
 <script type="text/javascript"> var Settings = {base_url: '<?= base_url(); ?>'}</script>
 <script type="text/javascript"> var SkillsSettings = {startValues : '<?= (isset($preFill)) ? $preFill : ""; ?>'}</script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo asset_url(); ?>/js/jQuery-slimScroll-1.0.6/jquery.slimscroll.min.js"></script>
-<script src="<?php echo asset_url() ?>/js/script.js" type="text/javascript"></script>
-<script src="<?php echo asset_url() ?>/js/tipsy.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>/js/jquery.autoSuggest.js" type="text/javascript"></script>
-<script src="<?php echo asset_url(); ?>/js/slides.min.jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo asset_url() ?>/js/script.js"></script>
+<script type="text/javascript" src="<?php echo asset_url() ?>/js/tipsy.js"></script>
+<script type="text/javascript" src="<?php echo asset_url(); ?>/js/jquery.autoSuggest.js"></script>
+<script type="text/javascript" src="<?php echo asset_url(); ?>/js/slides.min.jquery.js"></script>
 <script type="text/javascript" src="<?php echo asset_url(); ?>/js/jquery.leanModal.min.js"></script>
 <script type="text/javascript" src="<?php echo asset_url(); ?>/js/typeahead/typeahead.min.js"></script>
-<script src="<?php echo asset_url(); ?>/js/gcharts/skills_barchart.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php echo asset_url(); ?>/js/gcharts/skills_barchart.js"></script>
 <?php if (!empty($profile_missing)): ?>
-	<script type="text/javascript" src="<?php echo asset_url(); ?>/js/profileSuggest.js"></script>
+<script type="text/javascript" src="<?php echo asset_url(); ?>/js/profileSuggest.js"></script>
 <?php endif; ?>
 <script type="text/javascript">
 	$(function() {
@@ -82,7 +83,7 @@
         height: '220px'
     });
 </script>
-<!-- Google Analysitcs -->
+<!-- Google Analytics -->
 <script type="text/javascript">
 
     var _gaq = _gaq || [];
