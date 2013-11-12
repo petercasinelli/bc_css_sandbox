b6c10596197f8ad77979e6bcc06dda853369b46f
Peter Casinelli <pcas00@gmail.com>
2013-11-11 23:04:49 -0500

    Add AWS email config

--- application/config/email.php
+++ application/config/email.php
@@ -1,14 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$vars = get_cfg_var('aws.param5');
-$explode = explode(',', $vars);
-$pass = $explode[1];
+//Get Amazon configuration variables
+$config = get_cfg_var('aws.param2');
+$config = json_decode($config, TRUE);
+
+$email_config = $config['smtp'][0];
 
 $config['protocol']='smtp';
 $config['smtp_host']='ssl://smtp.googlemail.com';
 $config['smtp_port']=465;
 $config['smtp_timeout']='30';
 $config['smtp_user']='bccss.development@gmail.com';
-$config['smtp_pass']= $pass;
+$config['smtp_pass']= $email_config['password'];
 $config['charset']="utf-8";
 $config['newline']="\r\n";
